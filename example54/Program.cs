064cfc31719eb99ddcc1e092ede2b8e4f3a3eee2
unknown <perecpon@yandex.ru>
2022-10-14 17:01:54 +0300

    Sort

---
+++ example54/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int[,] GenerateArray(int hight, int wight, int deviation)
+{
+    int[,] generatedArray = new int[hight, wight];
+    for (int i = 0; i < hight; i++)
+    {
+        for (int j = 0; j < wight; j++)
+        {
+            generatedArray[i, j] = new Random().Next(deviation);
+        }
+    }
+    return generatedArray;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void showArray(int[,] inputArray)
+{
+    printColorData($" \t");
+    for (int i = 0; i < inputArray.GetLength(1); i++)
+    {
+        printColorData($"{i}\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        printColorData($"{i}\t");
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write($"{inputArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SortToLower(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputArray.GetLength(1) - 1; k++)
+            {
+                if (inputArray[i, k] < inputArray[i, k + 1])
+                {
+                    int bufer = inputArray[i, k + 1];
+                    inputArray[i, k + 1] = inputArray[i, k];
+                    inputArray[i, k] = bufer;
+                }
+            }
+        }
+    }
+   return inputArray;
+}
+int[,] generatedArray = GenerateArray(3, 5, 100);
+showArray(generatedArray);
+int[,] swoppedArray = SortToLower(generatedArray);
+showArray(swoppedArray);
